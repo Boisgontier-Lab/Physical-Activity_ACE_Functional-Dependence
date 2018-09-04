02590176adda278b9423e25d36a75a4765eb366a
Dan <dan.orsholits@gmail.com>
2018-09-04 14:49:15 +0200

    Initial commit of master R script file.

---
+++ R/SHARE_ACE_FunctionalDependence.R
@@ -0,0 +1,6 @@
+#### This is the master script that imports data and runs the anylses
+
+### This calls a script that imports the raw SHARE data
+### It assumes that the data is in CSV format
+
+source(file = "ImportSHARECSV.R")
