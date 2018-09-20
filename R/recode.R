7c916964acd0256d12b9a3e6a1ddf20d1b29f636
Dan <dan.orsholits@gmail.com>
2018-09-20 15:42:12 +0200

    In progress version of the general recode script.

---
+++ R/recode.R
@@ -0,0 +1,28 @@
+#### This is the script which recodes and creates new variables
+
+### Load the "subsetted" data
+load(file = "../data/shareAllSubset.RData")
+
+### Load script containing some useful basic functions
+source(file = "recodeFunctions.R")
+
+### Clean up all columns
+shareAllSubsetRecode <- as.data.frame(lapply(shareAllSubset,
+                                             SetMissing))
+                                      
+### Any ".x" columns should be made stripped of the suffix
+names(shareAllSubsetRecode) <- gsub("\\.x", "",
+                                    names(shareAllSubsetRecode))
+
+### Any ".y" columns should be denoted as applying to wave 3
+names(shareAllSubsetRecode) <- gsub("\\.y", ".w3",
+                                    names(shareAllSubsetRecode))
+
+### Generate dropout indicator
+source(file = "dropout.R")
+
+### Generate sociodemographic variables
+source(file = "socioDemo.R")
+
+### Generate childhood socioeconomic condition variables
+source(file = "childSEC.R")
