3a739b80bc4eab76d881d613d7a6f76da256bc7a
Dan <dan.orsholits@gmail.com>
2018-10-25 16:18:26 +0200

    Use integers instead of logical and fix deathParents variable creation.

--- R/ACE.R
+++ R/ACE.R
@@ -26,13 +26,13 @@ shareAllSubsetRecode$deathMother15 <-
 ### Assign a score making construction of ACE score easier; if neither parents
 ### perished before or at age 15 score of 0, one parent score of 1, both parents
 ### score of 2
-shareAllSubsetRecode$deathParents15 <- 0
+shareAllSubsetRecode$deathParents15 <- 0L
 shareAllSubsetRecode[which(shareAllSubsetRecode$deathMother15 |
                            shareAllSubsetRecode$deathFather15),
-                     "deathParents15"] <- 1
+                     "deathParents15"] <- 1L
 shareAllSubsetRecode[which(shareAllSubsetRecode$deathMother15 &
                            shareAllSubsetRecode$deathFather15),
-                     "deathParents15"] <- 2
+                     "deathParents15"] <- 2L
 shareAllSubsetRecode[which(is.na(shareAllSubsetRecode$deathMother15) &
                            is.na(shareAllSubsetRecode$deathFather15)),
                      "deathParents15"] <- NA
@@ -43,11 +43,12 @@ shareAllSubsetRecode$deathParents15 <-
                              as.character(shareAllSubsetRecode$mergeid),
                              function(x) {
                                  if (all(is.na(x))) {
-                                     rep(NA, length(x))
+                                     NA
                                  } else {
-                                     rep(max(x, na.rm = TRUE), length(x))
+                                     max(x, na.rm = TRUE)
                                  }
-                             })
+                             }, simplify = FALSE),
+                      shareAllSubsetRecode$mergeid
                       )
 
 ### We don't need to duplicate info for the SHARELife modules as this was done
@@ -66,10 +67,10 @@ shareAllSubsetRecode$propertyAway15  <-
                  ub = 15)
 
 ### Respondent lived outside of parent's care
-shareAllSubsetRecode$livedInCare <- FALSE
+shareAllSubsetRecode$livedInCare <- 0L
 shareAllSubsetRecode[which(shareAllSubsetRecode$sl_ac002d1 == "Selected" |
                           shareAllSubsetRecode$sl_ac002d2 == "Selected"),
-                     "livedInCare"] <- TRUE
+                     "livedInCare"] <- 1L
 shareAllSubsetRecode[which(is.na(shareAllSubsetRecode$sl_ac002d1) &
                            is.na(shareAllSubsetRecode$sl_ac002d2)),
                      "livedInCare"] <- NA
@@ -77,16 +78,16 @@ shareAllSubsetRecode[which(is.na(shareAllSubsetRecode$sl_ac002d1) &
 ### Parents had drinking problems
 shareAllSubsetRecode$parentsDrinkingProb <- NA
 shareAllSubsetRecode[which(shareAllSubsetRecode$sl_hs045d2 == "Not selected"),
-                     "parentsDrinkingProb"] <- FALSE
+                     "parentsDrinkingProb"] <- 0L
 shareAllSubsetRecode[which(shareAllSubsetRecode$sl_hs045d2 == "Selected"),
-                     "parentsDrinkingProb"] <- TRUE
+                     "parentsDrinkingProb"] <- 1L
 
 ### Parents had mental problems
 shareAllSubsetRecode$parentsMentalProb <- NA
 shareAllSubsetRecode[which(shareAllSubsetRecode$sl_hs045d3 == "Not selected"),
-                     "parentsMentalProb"] <- FALSE
+                     "parentsMentalProb"] <- 0L
 shareAllSubsetRecode[which(shareAllSubsetRecode$sl_hs045d3 == "Selected"),
-                     "parentsMentalProb"] <- TRUE
+                     "parentsMentalProb"] <- 1L
 
 ### The composite adverse childhood events indicator
 shareAllSubsetRecode$ACEScore15 <-
